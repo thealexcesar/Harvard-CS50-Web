d34bc5f8eb73e2bdbc3f3d1953ceee48aaac91a8
alexcesarsilva <alexcesars@icloud.com>
2021-06-19 16:03:16 -0300

    using string

--- Knowledge/Django/prototype/hello/views.py
+++ Knowledge/Django/prototype/hello/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return HttpResponse("<h1 style=\"background:#f9d8a8\">Hello, world!</h1><p style=\"font-family:sans-serif\">Using Django.</p>")
 def greet(request, name):
     return HttpResponse(f"Hello, {name.capitalize()}!")
