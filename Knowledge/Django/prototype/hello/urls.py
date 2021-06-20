235f3dd9abd1182ab02d6612a50d2f68cae5141d
alexcesarsilva <alexcesars@icloud.com>
2021-06-20 12:58:06 -0300

    Tasks

--- Knowledge/Django/prototype/hello/urls.py
+++ Knowledge/Django/prototype/hello/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"), 
-    path("<str:name>", views.greet, name="greet")
+    path("", views.index, name="index")
 ]
